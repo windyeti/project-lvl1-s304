2a02731110457d3d53cd0a7a3a3ea20b35aa2e06
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-07-12 14:18:37 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-    const response = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${response}`);
-}
+  const response = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${response}`);
+};
