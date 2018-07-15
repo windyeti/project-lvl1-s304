970030a05e22f7a5f405605e57fb135aeb8b79ce
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-07-15 14:53:14 +0300

    chore(cons,car,cdr) games

--- src/games/gameCalc.js
+++ src/games/gameCalc.js
@@ -1,27 +1,27 @@
 import templGame from '../templGame';
+import { cons, car, cdr, toString } from 'hexlet-pairs';
 
-export default () => {
-  const rule = 'What is the result of the expression?';
-  const question = () => {
-    const arrAction = ['+', '-', '*'];
-    const randomIndAction = Math.ceil(Math.random() * 3) - 1;
-    const action = arrAction[randomIndAction];
-    const num1 = Math.ceil(Math.random() * 100);
-    const num2 = Math.ceil(Math.random() * 100);
-    return `${num1} ${action} ${num2}`;
-  };
-  const rightAnswer = (currentQuestion) => {
-    const arrCurrentQuestion = currentQuestion.split(' ');
-    switch (arrCurrentQuestion[1]) {
+const rule = 'What is the result of the expression?';
+const question = () => {
+  const arrAction = ['+', '-', '*'];
+  const randomIndAction = Math.ceil(Math.random() * 3) - 1;
+  const action = arrAction[randomIndAction];
+  const num1 = Math.ceil(Math.random() * 100);
+  const num2 = Math.ceil(Math.random() * 100);
+  const str = `${num1} ${action} ${num2}`;
+  const sum = () => {
+    switch (action) {
       case '+':
-        return (Number(arrCurrentQuestion[0]) + Number(arrCurrentQuestion[2])).toString();
+        return (num1 + num2).toString();
       case '-':
-        return (Number(arrCurrentQuestion[0]) - Number(arrCurrentQuestion[2])).toString();
+        return (num1 - num2).toString();
       case '*':
-        return (Number(arrCurrentQuestion[0]) * Number(arrCurrentQuestion[2])).toString();
+        return (num1 * num2).toString();
       default:
         return false;
     }
   };
-  templGame(rule, question, rightAnswer);
+  return cons(str, sum());
 };
+
+export default () => templGame(rule, question);
--- src/templGame.js
+++ src/templGame.js
@@ -1,20 +1,18 @@
 import readlineSync from 'readline-sync';
+import { car, cdr } from 'hexlet-pairs';
 
-import greet from './index';
+import greet from '.';
 
-export default (rule, getQuestion, getRightAnswer) => {
+export default (rule, getQuestion) => {
   console.log('Welcome to the Brain Games!');
-  if (rule) console.log(rule);
+  console.log(rule);
   const name = greet();
-  if (!getQuestion) return;
-  const timeTry = 3;
-  for (let i = 0; i < timeTry; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     const currentQuestion = getQuestion();
-    console.log(`Question: ${currentQuestion}`);
-    const rightAnswer = getRightAnswer(currentQuestion);
+    console.log(`Question: ${car(currentQuestion)}`);
+    const rightAnswer = cdr(currentQuestion);
     const currentAnswer = readlineSync.question('Your answer: ');
-    const isCorrect = () => currentAnswer === rightAnswer;
-    if (isCorrect()) {
+    if (currentAnswer === rightAnswer) {
       console.log('Correct');
     } else {
       console.log(`'${currentAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}`);
