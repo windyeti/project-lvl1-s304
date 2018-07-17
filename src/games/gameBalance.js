4f884edc0cae37e202c7debfb6721b6b96a30cc8
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-07-17 14:03:13 +0300

    chore brain-balance

--- src/games/gameBalance.js
+++ src/games/gameBalance.js
@@ -5,44 +5,31 @@ import getRandomNumber from '../utils';
 
 const rule = 'Balance the given number.';
 
-const changeArrNum = (arr) => {
-  let max = arr[arr.length - 1];
-  let maxInd = 0;
+const rebuildArrNum = (arr) => {
+  let max = arr[0];
   let min = arr[0];
-  let minInd = 0;
-  for (let i = arr.length - 2; i >= 0; i -= 1) {
+
+  for (let i = 1; i < arr.length; i += 1) {
     if (max < arr[i]) {
       max = arr[i];
-      maxInd = i;
     }
-    console.log('max', max);
-  }
-  for (let i = 1; i < arr.length; i += 1) {
     if (arr[i] < min) {
       min = arr[i];
-      minInd = i;
     }
-    console.log('min', min);
   }
-  console.log('TYPEOF', typeof arr[maxInd]);
-
   const newArr = [].slice.call(arr);
-  newArr.splice(maxInd, 1, arr[maxInd] - 1);
-  newArr.splice(minInd, 1, arr[minInd] + 1);
+  newArr.splice(arr.indexOf(max), 1, arr[arr.indexOf(max)] - 1);
+  newArr.splice(arr.indexOf(min), 1, arr[arr.indexOf(min)] + 1);
 
-  console.log('arr[maxInd]', newArr[maxInd], maxInd);
-  console.log('arr[minInd]', newArr[minInd], minInd);
   return newArr;
 };
 
-const isOne = arr => Math.max(...arr) - Math.min(...arr) === 1;
+const isDiffMoreOne = arr => Math.max(...arr) - Math.min(...arr) > 1;
 
 const getBalance = (num) => {
   let arrNum = num.toString().split('').map(Number);
-  console.log('перед while', arrNum);
-  while (!isOne(arrNum)) {
-    arrNum = changeArrNum(arrNum);
-    console.log('внутри while', arrNum);
+  while (isDiffMoreOne(arrNum)) {
+    arrNum = rebuildArrNum(arrNum);
   }
   console.log('после while', arrNum.sort().join(''));
   return arrNum.sort().join('');
@@ -50,7 +37,6 @@ const getBalance = (num) => {
 
 const question = () => {
   const randomNumber = getRandomNumber(10000);
-  // console.log('randomNumber', randomNumber);
   const num = getBalance(randomNumber);
   return cons(randomNumber.toString(), num);
 };
